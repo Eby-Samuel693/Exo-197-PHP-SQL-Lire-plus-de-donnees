06d4ebb61eec77f4879e88a4c83e799ef2aa41cf
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-03-03 09:25:09 +0100

    vtby

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
     <title>Exo complet lecture SQL.</title>
 </head>
 <body>
+    <?php
+        $server = 'localhost';
+        $db = 'exo_197';
+        $user = 'root';
+        $pass = '';
+
+        $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8",$user,$pass);
+    ?>
 
 </body>
 </html>
+
+
